71244e61e15ad8eb645c453be38729c3aeda6d41
Sungpil An <ansungpil1@gmail.com>
2021-02-16 13:33:41 -0500

    Added img and header.js

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/breakingbadlogo.png'
+
+const Header = () => {
+    return (
+       <header className='center'>
+           <img src={logo} al='' />
+       </header>
+    )
+}
+
+export default Header
