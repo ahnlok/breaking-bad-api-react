ca8aa142089d61e2c0a7254f1978984a6b6e5937
Sungpil An <ansungpil1@gmail.com>
2021-02-16 14:17:47 -0500

    created spinner.js and imported into chracteritem.js

---
+++ src/components/ui/Search.js
@@ -0,0 +1,28 @@
+import { getSuggestedQuery } from '@testing-library/react'
+import React, { useState } from 'react'
+
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+
+    return (
+        <section className='search'>
+            <form>
+                <input 
+                    type="text"
+                    className="form-control"
+                    placeholder="Search Characters"
+                    value={text}
+                    onChange={(e) => onChange(e.target.value)}
+                    autoFocus
+                />
+            </form>
+        </section>
+    )
+}
+
+export default Search;
